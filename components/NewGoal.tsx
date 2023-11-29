ad1099a441d9181c8a6052215d202702dcb906ed
aashik <aashikdevelop@gmail.com>
2023-11-29 15:01:51 +0530

    Working with Generic Event Types

--- components/NewGoal.tsx
+++ components/NewGoal.tsx
@@ -1,6 +1,8 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 const NewGoal = () => {
+  const goalRef = useRef(null);
+  const summaryRef = useRef(null);
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
   }
@@ -8,11 +10,16 @@ const NewGoal = () => {
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your Goal</label>
-        <input type="text" placeholder="Your Goal..." id="goal" />
+        <input type="text" placeholder="Your Goal..." id="goal" ref={goalRef} />
       </p>
       <p>
         <label htmlFor="summary">Short Summary</label>
-        <input type="text" placeholder="Short Summary..." id="summary" />
+        <input
+          type="text"
+          placeholder="Short Summary..."
+          id="summary"
+          ref={summaryRef}
+        />
       </p>
       <button>Add Goal</button>
     </form>
