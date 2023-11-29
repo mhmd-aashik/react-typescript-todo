208b68f741831859f5a6c43f14e690f37b9f75e5
aashik <aashikdevelop@gmail.com>
2023-11-29 13:00:02 +0530

    Working with State & Outputting State-based Values

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,14 @@ export default function App() {
   // altenative <Array<CourseGoal>>
   const [goals, setGoals] = useState<CourseGoal[]>([]);
   function handleAddGoal() {
-    setGoals();
+    setGoals((prevState) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it from the ground up",
+      };
+      return [...prevState, newGoal];
+    });
   }
   return (
     <main>
@@ -21,9 +28,15 @@ export default function App() {
         <h1>Your Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal</button>
-      <CourseGoal title="Learn React + TS">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
